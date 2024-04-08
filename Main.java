eeb30430015704faaede597497f322559600eb5d
VinhCong <congvinh2701@gmail.com>
2024-04-08 21:31:32 +0700

    first project

---
+++ Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("Họ và tên: Nguyễn Công Vinh");
+       System.out.println("MSV:23020445");
+       System.out.println("Lớp K68-AI1");
+       System.out.println("Username:NgCVinh");
+       System.out.println("Địa chỉ email:congvinh2701@gmail.com");
+       for(int i = 9; i >= 1; i--){
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+       }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
